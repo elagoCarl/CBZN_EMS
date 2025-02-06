93ee92ed46efec40b2c5e0d36db75002d28b041c
Ro Padilla <rouenpads@gmail.com>
2025-02-06 15:01:04 +0800

    loginPage.jsx added

--- Client/Components/loginPage.jsx
+++ Client/Components/loginPage.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const loginPage = () => {
   return (
-    <div>
+    <div class="bg-gray-700 h-screen">
       <div>
         <nav>
-          <div class ="mx-auto max-w-7xl">
+          <div class ="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <img src="" alt="" />
           </div>
         </nav>
