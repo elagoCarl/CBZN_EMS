ea711592b8370f36fd6068b47a8c70549d7ae01b
Ro Padilla <rouenpads@gmail.com>
2025-02-06 13:29:01 +0800

    app.jsx and loginPage.jsx created

---
+++ Client/Components/loginPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const loginPage = () => {
+  return (
+    <div>
+      <div>
+        <nav>
+          <div class ="mx-auto max-w-7xl">
+            <img src="" alt="" />
+          </div>
+        </nav>
+      </div>
+      </div>
+  )
+}
+
+export default loginPage
