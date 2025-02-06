c40dbce99d70f3a5271b9176b331da78b3cfb7cd
Ro Padilla <rouenpads@gmail.com>
2025-02-06 17:42:34 +0800

    loginPage.jsx

---
+++ Client/tailwindcss4/src/Components/loginPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="bg-green-300 bg-cover">Login</h1>
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
