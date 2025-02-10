491cd4f7df4d733ceb633f5d5fc54c2fa87348e0
icmags <157679993+icmags@users.noreply.github.com>
2025-02-10 17:34:03 +0800

    Add EditUser Page

--- Client/tailwindcss4/src/App.jsx
+++ Client/tailwindcss4/src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from './Components/loginPage.jsx';
+import EditUser from './Components/editUSer.jsx';
 
 
 function App() {
   const [count, setCount] = useState(0)
   return (
-    <>
-      <div>
-        <LoginPage />
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/editUser" element={<EditUser />} />
+      </Routes>
+    </Router>
   )
 }
 
