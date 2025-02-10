58f5cb55abee5386b6f8993c553707eada33456d
Ro Padilla <rouenpads@gmail.com>
2025-02-10 13:44:04 +0800

    EmployeeHome on works

--- Client/tailwindcss4/src/App.jsx
+++ Client/tailwindcss4/src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from './Components/loginPage.jsx';
+import EmployHome from './Components/employHome.jsx';
 
 
 function App() {
   const [count, setCount] = useState(0)
   return (
     <>
-      <div>
-        <LoginPage />
-      </div>
+      <Router>
+        <Routes>
+        <Route path="/loginPage" element={<LoginPage />} />
+        <Route path="/EmployHome" element={<EmployHome />} />
+        </Routes>
+      </Router>
     </>
   )
 }
