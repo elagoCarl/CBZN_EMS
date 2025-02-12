f0e9324cf2f0bfb7b512a57bc9e10e92e19ae37a
icmags <157679993+icmags@users.noreply.github.com>
2025-02-12 11:41:20 +0800

    Modify the addUser, editUser, and its path

--- Client/tailwindcss4/src/App.jsx
+++ Client/tailwindcss4/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from './Components/loginPage.jsx';
-import EditUser from './Components/editUSer.jsx';
+import EditUser from './Components/callComponents/editUser.jsx';
 import EmployeeHome from './Components/employHome.jsx';
-import AddUser from './Components/addUser.jsx';
+import AddUser from './Components/callComponents/addUser.jsx';
 
 
 function App() {
@@ -10,10 +10,10 @@ function App() {
     <>
       <Router>
         <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/employeeHome" element={<EmployeeHome />} />
-        <Route path="/addUser" element={<AddUser />} />
-        {/* <Route path="/attendancePage" element={<AttendancePage />} /> */}
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/employeeHome" element={<EmployeeHome />} />
+          <Route path="/addUser" element={<AddUser />} />
+          {/* <Route path="/attendancePage" element={<AttendancePage />} /> */}
           <Route path="/editUser" element={<EditUser />} />
         </Routes>
       </Router>
