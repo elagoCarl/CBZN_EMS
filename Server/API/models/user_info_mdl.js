dc51280bc14f071aed7ff827f6eae8b9833225e1
elagoCarl <157703384+elagoCarl@users.noreply.github.com>
2025-02-13 17:35:30 +0800

    user, userinfo,emgncyContact mdls

---
+++ Server/API/models/user_info_mdl.js
@@ -0,0 +1,57 @@
+const bcrypt = require('bcrypt');
+
+module.exports = (sequelize, DataTypes) => {
+    const UserInfo = sequelize.define('UserInfo', {
+        age : { 
+            type: DataTypes.INTEGER, 
+            allowNull: false 
+        },
+        city_add: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        provincial_add: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        birthdate: {
+            type: DataTypes.DATEONLY,
+            allowNull: false
+        },
+        civil_status: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        name_of_spouse: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        spouse_occupation: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        employed_by: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        father_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        father_occupation: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+    }, {
+        timestamps: true,
+    });
+    User.associate = (models) => {
+        User.belongsTo(models.Schedule, {
+        });
+        User.hasOne(models.Attendance, {
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE'
+        });
+    };
+    return UserInfo;
+};
