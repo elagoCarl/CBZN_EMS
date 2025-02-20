da65f903d2e58909fbaa3a2e1d22039d0795f595
elagoCarl <157703384+elagoCarl@users.noreply.github.com>
2025-02-20 11:56:25 +0800

    integrated addUser component

--- Client/tailwindcss4/src/App.jsx
+++ Client/tailwindcss4/src/App.jsx
@@ -4,7 +4,8 @@ import ForgotPass from './Components/forgotPass.jsx';
 import AdminDashboard from "./Components/adminDashboard.jsx"; 
 import EmployeeDashboard from "./Components/employeeDashboard.jsx";
 import { Req } from './Components/callComponents/Req.jsx';
-// import EmployeeHome from './Components/employHome.jsx';
+import Sidebar from './Components/callComponents/sidebar.jsx';
+import EmployeeHome from './Components/employHome.jsx';
 // import AdminEms from './Components/admin_Ems.jsx'
 import AddUser from './Components/callComponents/addUser.jsx';
 
@@ -17,10 +18,11 @@ function App() {
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/addUser" element={<AddUser />} />
-          {/* <Route path="/employeeHome" element={<EmployeeHome />} /> */}
+          <Route path="/employeeHome" element={<EmployeeHome />} />
           {/* <Route path="/attendancePage" element={<AttendancePage />} /> */}
           {/* <Route path="/adminEms" element={<AdminEms />} /> */}
           <Route path="/req" element={<Req />} />
+          <Route path="/sidebar" element={<Sidebar />} />
           <Route path="/forgotPass" element={<ForgotPass />} />
           <Route path="/adminDashboard" element={<AdminDashboard />} />
           <Route path="/employeeDashboard" element={<EmployeeDashboard />} />
