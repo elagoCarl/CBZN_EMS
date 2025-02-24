a0264608a5bbf036b0ddedda826e9f005d8db30e
Ro Padilla <rouenpads@gmail.com>
2025-02-24 14:50:24 +0800

    PR
    PR

--- Client/tailwindcss4/src/App.jsx
+++ Client/tailwindcss4/src/App.jsx
@@ -4,15 +4,15 @@ import ForgotPass from './Components/forgotPass.jsx';
 import AdminDashboard from "./Components/adminDashboard.jsx"; 
 import EmployeeDashboard from "./Components/employeeDashboard.jsx";
 import { Req } from './Components/callComponents/Req.jsx';
-// import EmployeeHome from './Components/employHome.jsx';
-// import AdminEms from './Components/admin_Ems.jsx'
 import AddUser from './Components/callComponents/addUser.jsx';
 import EmployeeSettings from './Components/employeeSettings.jsx';
 import AdminSettings from './Components/adminSettings.jsx';
 import AdminAttendance from './Components/adminAttendance.jsx';
-import EmployeeAttendance from './Components/employeeAttendance.jsx';
 import ReqPage from './Components/reqPage.jsx';
-import Sidebar from './Components/sidebar.jsx';
+import Sidebar from './Components/callComponents/sidebar.jsx';
+// import EmployeeHome from './Components/employHome.jsx';
+// import AdminEms from './Components/admin_Ems.jsx'
+// import EmployeeAttendance from './Components/employeeAttendance.jsx';
 
 function App() {
   return (
@@ -21,9 +21,6 @@ function App() {
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/addUser" element={<AddUser />} />
-          {/* <Route path="/employeeHome" element={<EmployeeHome />} /> */}
-          {/* <Route path="/attendancePage" element={<AttendancePage />} /> */}
-          {/* <Route path="/adminEms" element={<AdminEms />} /> */}
           <Route path="/req" element={<Req/>} />
           <Route path="/reqPage" element={<ReqPage />} />
           <Route path="/sidebar" element={<Sidebar />} />
@@ -33,7 +30,10 @@ function App() {
           <Route path="/employeeSettings" element={<EmployeeSettings />} />
           <Route path="/adminSettings" element={<AdminSettings />} />
           <Route path="/adminAttendance" element={<AdminAttendance />} />
-          <Route path="/employeeAttendance" element={<EmployeeAttendance />} />
+          {/* <Route path="/employeeHome" element={<EmployeeHome />} /> */}
+          {/* <Route path="/attendancePage" element={<AttendancePage />} /> */}
+          {/* <Route path="/adminEms" element={<AdminEms />} /> */}
+          {/* <Route path="/employeeAttendance" element={<EmployeeAttendance />} /> */}
       </Routes>
       </Router>
     </>
