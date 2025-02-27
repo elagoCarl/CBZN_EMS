c2ee19f61ac911c22069925b1df7c3d2e4424546
02pablo <pablo2121854@mkt.ceu.edu.ph>
2025-02-27 11:44:32 +0800

    updated sched change

--- Client/tailwindcss4/src/App.jsx
+++ Client/tailwindcss4/src/App.jsx
@@ -1,18 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from './Components/loginPage.jsx';
 import ForgotPass from './Components/forgotPass.jsx';
-import AdminDashboard from "./Components/adminDashboard.jsx"; 
-import EmployeeDashboard from "./Components/myAttendance.jsx"
+import ManageUsers from './Components/manageUsers.jsx';
+import MyAttendance from "./Components/myAttendance.jsx";
 import Sidebar from './Components/callComponents/sidebar.jsx';
-import AdminSettings from './Components/adminSettings.jsx';
+import AccountSettings from './Components/accSettings.jsx';
 import AttendanceList from './Components/attendanceList.jsx';
 import ReqPage from './Components/reqPage.jsx';
 import DeptPage from "./Components/departmentPage.jsx";
 import ScheduleChangePage from "./Components/scheduleChangePage.jsx";
-import MyAttendance from "./Components/myAttendance.jsx";
 import TimeAdjustmentPage from "./Components/timeAdjusmentPAge.jsx";
-import OTReqPage from "./Components/otReqPage.jsx";
-import LeaveReqPage from "./Components/leaveReqPage.jsx";
+// import OTReqPage from "./Components/otReqPage.jsx";
+// import LeaveReqPage from "./Components/leaveReqPage.jsx";
 
 function App() {
   return (
@@ -22,17 +21,17 @@ function App() {
           <Route path="/" element={<LoginPage />} />
           <Route path="/reqPage" element={<ReqPage />} />
           <Route path="/forgotPass" element={<ForgotPass />} />
-          <Route path="/adminDashboard" element={<AdminDashboard />} />
-          <Route path="/employeeDashboard" element={<EmployeeDashboard />} />
-          <Route path="/adminSettings" element={<AdminSettings />} />
+          <Route path="/manageUsers" element={<ManageUsers />} />
+          <Route path="/myAttendance" element={<MyAttendance />} />
+          <Route path="/accSettings" element={<AccountSettings />} />
           <Route path="/attendanceList" element={<AttendanceList />} />
           <Route path="/deptPage" element={<DeptPage />} />
           <Route path="/myAttendance" element={<MyAttendance />} />
           <Route path="/scheduleChangePage" element={<ScheduleChangePage />} />
           <Route path="/sidebar" element={<Sidebar />} />
           <Route path="/timeAdjustmentPage" element={<TimeAdjustmentPage />} />
-          <Route path="/otReqPage" element={<OTReqPage />} />
-          <Route path="/leaveReqPage" element={<LeaveReqPage />} />
+          {/* <Route path="/otReqPage" element={<OTReqPage />} />
+          <Route path="/leaveReqPage" element={<LeaveReqPage />} /> */}
       </Routes>
       </Router>
     </>
