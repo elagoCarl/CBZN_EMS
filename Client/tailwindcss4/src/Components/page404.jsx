244b810dd7ff56b3fb2709db4428468c0614aa1a
Ro Padilla <rouenpads@gmail.com>
2025-03-03 17:20:37 +0800

    page404

--- Client/tailwindcss4/src/Components/page404.jsx
+++ Client/tailwindcss4/src/Components/page404.jsx
@@ -7,7 +7,7 @@ const Page404 = () => {
   return (
     <div className="flex justify-center items-center h-screen px-4 bg-black/85">
       <div className="text-center">
-        <Frown className="mx-auto my-4 text-gray-400" size={120} /> {/* Sad face icon */}
+        <Frown className="mx-auto text-gray-400" size={120} /> {/* Sad face icon */}
         <h1 className="text-9xl font-bold text-gray-300">404</h1>
         <h2 className="text-2xl font-semibold text-gray-300">Page Not Found</h2>
         <p className="text-2xl md:text-2xl font-light text-gray-300 mt-4">
@@ -15,7 +15,7 @@ const Page404 = () => {
         </p>
         <button
           onClick={() => navigate("/")}
-          className="mt-6 px-6 py-3 bg-green-600 text-white text-lg font-semibold rounded-md hover:bg-green-800 transition duration-300 border-none animate-pulse"
+          className="mt-6 px-6 py-3 bg-green-600 text-white text-lg font-semibold rounded-md hover:bg-green-800 transition duration-300 border-none"
         >
           Go Back?
         </button>
