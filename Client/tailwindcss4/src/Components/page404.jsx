a283325431ab3d11bc3274dda3e6b19843159519
Ro Padilla <rouenpads@gmail.com>
2025-03-03 17:18:12 +0800

    rendering page404

--- Client/tailwindcss4/src/Components/page404.jsx
+++ Client/tailwindcss4/src/Components/page404.jsx
@@ -1,19 +1,21 @@
 import { useNavigate } from "react-router-dom";
+import { Frown } from "lucide-react";
 
 const Page404 = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex justify-center items-center h-screen bg-white px-4">
+    <div className="flex justify-center items-center h-screen px-4 bg-black/85">
       <div className="text-center">
-        <h1 className="text-9xl font-bold text-gray-800">404</h1>
-        <h2 className="text-2xl font-semibold text-gray-600">Page Not Found</h2>
-        <p className="text-2xl md:text-2xl font-light text-gray-600 mt-4">
+        <Frown className="mx-auto my-4 text-gray-400" size={120} /> {/* Sad face icon */}
+        <h1 className="text-9xl font-bold text-gray-300">404</h1>
+        <h2 className="text-2xl font-semibold text-gray-300">Page Not Found</h2>
+        <p className="text-2xl md:text-2xl font-light text-gray-300 mt-4">
           The page you are looking for doesn&apos;t exist.
         </p>
         <button
           onClick={() => navigate("/")}
-          className="mt-6 px-6 py-3 bg-blue-500 text-white text-lg font-semibold rounded-md hover:bg-blue-700 transition duration-300"
+          className="mt-6 px-6 py-3 bg-green-600 text-white text-lg font-semibold rounded-md hover:bg-green-800 transition duration-300 border-none animate-pulse"
         >
           Go Back?
         </button>
