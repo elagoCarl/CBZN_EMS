a4f05cb12e0607320fca826826e054855a1e4b4a
Kyla ~ <157708152+Kyla-G@users.noreply.github.com>
2025-03-27 17:53:53 +0800

    fixed userSched logic

--- Server/API/routers/sched_user_rtr.js
+++ Server/API/routers/sched_user_rtr.js
@@ -7,7 +7,9 @@ const { requireAuth } = require('../controllers/authMiddleware')
 router.post('/addSchedUser', SchedUser.addSchedUser);
 router.get('/getAllSchedUsers', SchedUser.getAllSchedUser);
 router.get('/getSchedUserById/:id', SchedUser.getSchedUser);
-router.put('/updateSchedUserByUser/:userId', SchedUser.updateSchedUserByUser);
+router.post('/updateSchedUserByUser/:userId', SchedUser.updateSchedUserByUser);
 router.get('/getSchedUserByUser/:userId', SchedUser.getSchedUserByUser);
 router.post('/getSchedUsersByUserCutoff/:userId', SchedUser.getSchedUsersByUserCutoff);
+router.get('/getAllSchedsByUser/:userId', SchedUser.getAllSchedsByUser);
+
 module.exports = router;
